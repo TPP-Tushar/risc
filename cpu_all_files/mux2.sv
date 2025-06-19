35f7d633ff03b81f78f5dbfd4672b0f1da044616
TPP <167067837+TPP-Tushar@users.noreply.github.com>
2025-06-20 01:54:32 +0530

    Delete cpu_all_files directory

--- cpu_all_files/mux2.sv
+++
@@ -1,8 +0,0 @@
-module mux2 #(parameter WIDTH = 16) (
-    input  logic             sel,
-    input  logic [WIDTH-1:0] in0,
-    input  logic [WIDTH-1:0] in1,
-    output logic [WIDTH-1:0] out
-);
-    assign out = sel ? in1 : in0;
-endmodule
